dae2a8e2d1c0a4539eee3571141b1f6d9ff7e6b1
Graham Holden <graham@opensussex.net>
2014-02-14 19:45:58 +0000

    create the DrumSynth func in this taken out of the tamburi_wav

--- tamburi.go
+++ tamburi.go
@@ -3,6 +3,9 @@ package main
 import (
 	"github.com/opensussex/wave_tamburi"
 	"os"
+	"time"
+	"math"
+	"math/rand"
 )
 
 const (
@@ -19,9 +22,46 @@ func main() {
 
 	file := wave.CreateFile(PCM, 1, 44100, 8)
 	//file.AppendSine(440, 5000, 20)
-	file.AppendDrum(2, 0.9995, 70.0, 0.002, 1.0, 0.9998, 0.5);
+	file.AppendByte(DrumSynth(2, 0.9995, 70.0, 0.002, 1.0, 0.9998, 0.5));
 	err = file.Write(fd)
 	if err != nil {
 		println(err.Error())
 	}
+}
+
+
+func DrumSynth( amp, decay, freq, freqdecay, noise, noisedecay, noisefilter float64 ) []byte{
+	var i, datalen uint32
+	var byte_data []byte
+	datalen = 44100
+
+	rand.Seed( time.Now().UTC().UnixNano())
+	var f float64
+	for i = 0; i < datalen; i++ {
+		data := amp * math.Sin( 2.0 * math.Pi * freq * float64(i) / float64(datalen) )
+		f = (noisefilter) * f + (1.0-noisefilter) * noise * ( float64( rand.Intn(100))  / 100.0 - 0.5)
+		data += f
+
+		noise *= noisedecay
+		amp *= decay
+		freq -= freqdecay
+
+		if( noise < .0 ){
+			noise = .0
+		}
+		if( amp < .0 ){
+			amp = .0
+		}
+		if( freq < .0 ){
+			freq = .0
+		}
+		if( data < -1.0 ){
+			data = -1.0
+		}
+		if( data >  1.0 ) {
+			data =  1.0
+		}	
+		byte_data = append(byte_data,byte(data))
+	}
+	return byte_data
 }
