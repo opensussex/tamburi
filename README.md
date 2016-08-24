c7d982f7fcc5cf7a31cd539d00e50c518697a98f
Graham Holden <graham.holden@elucidat.com>
2016-08-24 15:59:32 +0100

    pass in args from command line

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ tamburi
 =======
 
 golang command line drum synth
+
+command line arguments are:
+        amp, decay, freq, freqdecay, noise, noisedecay, noisefilter
+
+run: 
+        go run tamburi.go 4 4 200.0 0.001 1.0 0.0998 0.4
+
+Some good values
+
+        4 4 200.0 0.001 1.0 0.0998 0.4
+        4 4 400.0 0.01 1.0 0.0998 0.4
+        2 1 400.0 0.1 1 0.1998 0.6
+        
--- tamburi.go
+++ tamburi.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"github.com/opensussex/wave_tamburi"
-	"os"
-	"time"
+	"fmt"
 	"math"
 	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/opensussex/wave_tamburi"
 )
 
 const (
@@ -22,46 +25,64 @@ func main() {
 
 	file := wave.CreateFile(PCM, 1, 44100, 8)
 	//file.AppendSine(440, 5000, 20)
-	file.AppendByte(DrumSynth(4, 1, 200.0, 0.002, 1.0, 0.9998, 0.5));
+	if len(os.Args) > 1 {
+		if len(os.Args) < 8 {
+			fmt.Println(`Not Enough Arguments!`)
+			fmt.Println(`example: 4 2 200.0 0.022 1.0 0.8998 0.3`)
+			os.Exit(1)
+		}
+		arg1, _ := strconv.ParseFloat(os.Args[1], 64)
+		arg2, _ := strconv.ParseFloat(os.Args[2], 64)
+		arg3, _ := strconv.ParseFloat(os.Args[3], 64)
+		arg4, _ := strconv.ParseFloat(os.Args[4], 64)
+		arg5, _ := strconv.ParseFloat(os.Args[5], 64)
+		arg6, _ := strconv.ParseFloat(os.Args[6], 64)
+		arg7, _ := strconv.ParseFloat(os.Args[7], 64)
+		file.AppendByte(DrumSynth(arg1, arg2, arg3, arg4, arg5, arg6, arg7))
+	} else {
+		file.AppendByte(DrumSynth(4, 2, 200.0, 0.022, 1.0, 0.8998, 0.3))
+	}
+
 	err = file.Write(fd)
 	if err != nil {
 		println(err.Error())
+	} else {
+		fmt.Println(`Drum sound written to - test.wav`)
 	}
 }
 
-
-func DrumSynth( amp, decay, freq, freqdecay, noise, noisedecay, noisefilter float64 ) []byte{
+func DrumSynth(amp, decay, freq, freqdecay, noise, noisedecay, noisefilter float64) []byte {
 	var i, datalen uint32
 	var byte_data []byte
 	datalen = 44100
 
-	rand.Seed( time.Now().UTC().UnixNano())
+	rand.Seed(time.Now().UTC().UnixNano())
 	var f float64
 	for i = 0; i < datalen; i++ {
-		data := amp * math.Sin( 2.0 * math.Pi * freq * float64(i) / float64(datalen) )
-		f = (noisefilter) * f + (1.0-noisefilter) * noise * ( float64( rand.Intn(100))  / 100.0 - 0.5)
+		data := amp * math.Sin(2.0*math.Pi*freq*float64(i)/float64(datalen))
+		f = (noisefilter)*f + (1.0-noisefilter)*noise*(float64(rand.Intn(100))/100.0-0.5)
 		data += f
 
 		noise *= noisedecay
 		amp *= decay
 		freq -= freqdecay
 
-		if( noise < .0 ){
+		if noise < .0 {
 			noise = .0
 		}
-		if( amp < .0 ){
+		if amp < .0 {
 			amp = .0
 		}
-		if( freq < .0 ){
+		if freq < .0 {
 			freq = .0
 		}
-		if( data < -1.0 ){
+		if data < -1.0 {
 			data = -1.0
 		}
-		if( data >  1.0 ) {
-			data =  1.0
-		}	
-		byte_data = append(byte_data,byte(data))
+		if data > 1.0 {
+			data = 1.0
+		}
+		byte_data = append(byte_data, byte(data))
 	}
 	return byte_data
 }
